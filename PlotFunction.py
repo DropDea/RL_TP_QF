2ee404e8c9c64c8a28c3cf3b30c5f3d2163d5f09
DropDea <85173317+DropDea@users.noreply.github.com>
2022-03-17 11:57:56 +0100

    Create PlotFunction.py

---
+++ PlotFunction.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def Plt (rewards):
+  
+  plt.plot(100*(np.arange(len(rewards)) + 1), rewards)
+  plt.xlabel('Episodes')
+  plt.ylabel('Average Reward')
+  plt.title('Average Reward vs Episodes')
+  plt.savefig('rewards.jpg')     
+  plt.close() 
