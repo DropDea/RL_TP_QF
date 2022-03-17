7b98661a693f1cb054027ef54a155571dad5fe27
DropDea <85173317+DropDea@users.noreply.github.com>
2022-03-17 12:03:27 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import gym
 
 #Import fonction 
 from QLearningFunction.py import QLearning
-from PlotFunction.py import P
+from PlotFunction.py import Plt
 
 # Import and initialize Mountain Car Environment
 env = gym.make('MountainCar-v0')
@@ -12,3 +12,8 @@ env.reset()
 
 # Run Q-learning algorithm
 rewards = QLearning(env, 0.2, 0.9, 0.8, 0, 5000)
+
+#Run plot rewards 
+Plt(rewards)
+
+
